01498d49b4c49c4b7a0177e349728d1a36f93c2f
Barba2k2 <lorenzo.dz@hotmail.com>
2023-05-17 14:39:43 -0300

    middleware de autenticaçao JWT

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 import express from 'express';
+import admin from 'firebase-admin';
+import { authenticationToken } from './middlewares/authenticate-jwt.js';
 
 const app = express();
 
-// REST API
+admin.initializeApp({
+    credential: admin.credential.cert("serviceAccountKey.json")
+});
 
-// GET
-app.get('/transactions', (request, response) => {
-    console.log('GET transaction');
-    response.json([{id: 1}]);
+app.get('/transactions', authenticationToken, (request, response) => {
+    admin.firestore()
+        .collection('transactions')
+        .where('user.uid', '==', request.user.uid)
+        .orderBy('date', 'desc')
+        .get()
+        .then(snapshot => {
+            const transactions = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                uid: doc.uid
+            }))
+            response.json(transactions);
+        })
 })
 
-app.listen(3000, () => {console.log('API rest iniciada em http://localhost:3000')})
+app.listen(3000, () => { console.log('API rest iniciada em http://localhost:3000') })
